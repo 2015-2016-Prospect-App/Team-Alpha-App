68105332f93bb7802c9cc3d5efb8691b10ce6dda
Tyler Wasniowski <Rooket6@yahoo.com>
2015-11-22 18:37:56 -0800

    Testing Commit

--- app/src/main/java/teamalpha/teamalphaapp/LoginActivity.java
+++ app/src/main/java/teamalpha/teamalphaapp/LoginActivity.java
@@ -345,7 +345,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                // finish();
+                // finish(); This causes the app to exit out, thus appearing to crash - Tyler
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
