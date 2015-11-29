eac67bcd06ff4c773ca94ca7a6231637e5acf66c
Tyler Wasniowski <Rooket6@yahoo.com>
2015-11-28 21:27:20 -0800

    Added buttons to menu. (no functionality, just front-end)

---
+++ app/src/main/java/teamalpha/teamalphaapp/MenuActivity.java
@@ -0,0 +1,20 @@
+package teamalpha.teamalphaapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
