e8f1b86fbe1ead1f8db1b5b97d437e77da1fed13
Teck <teck@odi.net>
2015-12-30 09:49:52 -0800

    Changed Friends List.java and friendslist.xml and strings.xml, not completed yet

--- app/src/main/java/teamalpha/teamalphaapp/FriendsListActivity.java
+++ app/src/main/java/teamalpha/teamalphaapp/FriendsListActivity.java
@@ -1,13 +1,108 @@
 package teamalpha.teamalphaapp;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.Contacts.People;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
-public class FriendsListActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class FriendsListActivity extends ListActivity {
+
+    private ListAdapter mAdapter;
+    public TextView pbContact;
+    public static String PBCONTACT;
+    public static final int ACTIVITY_EDIT = 1;
+    private static final int ACTIVITY_CREATE = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    // Called when the activity is first created.
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
+        Cursor C = getContentResolver().query(People.CONTENT_URI, null, null, null, null);
+        startManagingCursor(C);
+
+        String[] columns = new String[]{People.NAME};
+        int[] names = new int[]{R.id.row_entry};
+
+        mAdapter = new SimpleCursorAdapter(this, R.layout.mycontacts, C, columns, names);
+        setListAdapter(mAdapter);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    } // end onCreate()
+
+    // Called when contact is pressed
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Cursor C = (Cursor) mAdapter.getItem(position);
+        PBCONTACT = C.getString(C.getColumnIndex(People.NAME));
+
+        // RHS 05/06
+        //pbContact = (TextView) findViewById(R.id.myContact);
+        //pbContact.setText(new StringBuilder().append("b"));
+
+        Intent i = new Intent(this, NoteEdit.class);
+        startActivityForResult(i, ACTIVITY_CREATE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "FriendsList Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://teamalpha.teamalphaapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "FriendsList Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://teamalpha.teamalphaapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
+
+
