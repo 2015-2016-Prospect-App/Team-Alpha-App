0c8062c50ec6209f21973063cbbb783e2551ca4b
onein50million <dman.wart@gmail.com>
2015-12-30 20:28:54 -0800

    added basic backend functionality dialog for new user asking for a display name adds user to database

--- app/src/main/java/teamalpha/teamalphaapp/MenuActivity.java
+++ app/src/main/java/teamalpha/teamalphaapp/MenuActivity.java
@@ -1,19 +1,56 @@
 package teamalpha.teamalphaapp;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MenuActivity extends AppCompatActivity {
+
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("request", "started");
+        queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.51:2014/user-exists?token=" + GoogleLoginActivity.acct.getIdToken();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Boolean exists = Boolean.parseBoolean(response);
+                        if(!exists){
+                            DialogFragment newFragment = new getNameDialog();
+                            newFragment.show(getFragmentManager(),"name");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Request",error.toString());
+            }
+        });
+        queue.add(stringRequest);
     }
 
     public void openFindAMatch(View v) {
---
+++ app/src/main/java/teamalpha/teamalphaapp/getNameDialog.java
@@ -0,0 +1,57 @@
+package teamalpha.teamalphaapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Daniel on 12/23/2015.
+ */
+public class getNameDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final LayoutInflater inflater = getActivity().getLayoutInflater();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final View dialogView = inflater.inflate(R.layout.dialog_name, null);
+        builder.setView(dialogView)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText text = (EditText)dialogView.findViewById(R.id.usernameText);
+                        String name = text.getText().toString();
+                        RequestQueue queue = Volley.newRequestQueue(getActivity());
+                        Log.i("Dialog", "Name is " + name);
+                        String url = "http://192.168.1.51:2014/add-user?token=" + GoogleLoginActivity.acct.getIdToken() + "&name=" + name;
+                        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                                new Response.Listener<String>() {
+                                    @Override
+                                    public void onResponse(String response) {
+                                        Log.i("Dialog", response);
+                                    }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Log.i("Dialog", error.toString());
+                                    }
+                                });
+                        queue.add(stringRequest);
+                    }
+                });
+
+        return builder.create();
+    }
+}
